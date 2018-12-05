8f98c6dc36e1eba9f253b98873983f60374d2b9f
v-shhong <v-shhong@microsoft.com>
2018-12-05 20:27:34 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+
+//动态设置字体大小
+document.addEventListener('DOMContentLoaded',()=>{
+  const html=document.querySelector('html');
+
+  let fontSize=window.innerWidth/10>40?40:window.innerWidth/10;
+  html.style.fontSize=fontSize+'px';
+});
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
